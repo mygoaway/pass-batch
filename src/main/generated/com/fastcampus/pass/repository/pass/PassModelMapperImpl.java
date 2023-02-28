84d4a0c1d3c0f8eebd0be825a7e73b3e41808b3f
mygoaway <mygoaway2@gmail.com>
2023-02-28 15:29:33 +0900

    05. Batch 이용권 일괄 지급 강의 후 커밋

---
+++ src/main/generated/com/fastcampus/pass/repository/pass/PassModelMapperImpl.java
@@ -0,0 +1,35 @@
+package com.fastcampus.pass.repository.pass;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-02-28T15:05:59+0900",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.14 (Oracle Corporation)"
+)
+@Component
+public class PassModelMapperImpl implements PassModelMapper {
+
+    @Override
+    public PassEntity toPassEntity(BulkPassEntity bulkPassEntity, String userId) {
+        if ( bulkPassEntity == null && userId == null ) {
+            return null;
+        }
+
+        PassEntity passEntity = new PassEntity();
+
+        if ( bulkPassEntity != null ) {
+            passEntity.setStatus( status( bulkPassEntity.getStatus() ) );
+            passEntity.setRemainingCount( bulkPassEntity.getCount() );
+            passEntity.setPackageSeq( bulkPassEntity.getPackageSeq() );
+            passEntity.setStartedAt( bulkPassEntity.getStartedAt() );
+            passEntity.setEndedAt( bulkPassEntity.getEndedAt() );
+        }
+        if ( userId != null ) {
+            passEntity.setUserId( userId );
+        }
+
+        return passEntity;
+    }
+}
